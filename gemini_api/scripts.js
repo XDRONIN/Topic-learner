e37eba5b331aaec322311c617ad58be6b654140d
XDRONIN <akashprakashmattathil@gmail.com>
2024-05-30 02:02:22 +0530

    the api now works in the browser for now

--- gemini_api/scripts.js
+++ gemini_api/scripts.js
@@ -1,19 +1,26 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-const dotenv = require("dotenv");
-dotenv.config();
-// Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+// Fetch your API_KEY
+const API_KEY = "AIzaSyDYtZAFk7bBNAnQ3v3w78PnrfPcV_3nPGc";
+
+// Access your API key (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+// ...
+
+// The Gemini 1.5 models are versatile and work with most use cases
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 async function run() {
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-  const prompt = "Write a story about a fairy.";
+  const prompt =
+    "write a poem about the talk between a person and a spider he just killed";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
   console.log(text);
 }
-
 run();
